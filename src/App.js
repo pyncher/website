99072de9ce9ae650d741f530ba74e32a7c096805
George Hanlon <georgehanlon@live.com>
2024-03-08 11:27:11 +0000

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./routes/Home";
+import Gigs from "./routes/Gigs";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gigs" element={<Gigs />} />
+        </Routes>
       </header>
     </div>
   );
