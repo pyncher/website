707273298d8e60dc7b19e977eddbda3e3b18b268
George Hanlon <georgehanlon@live.com>
2025-01-08 18:36:43 +0000

    Update to use songkick embed

--- src/routes/Live.js
+++ src/routes/Live.js
@@ -1,13 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import gigData from "../content/gigs.json";
-import "./Live.css"
-
-const gigList = gigData.map((gig) => {
-  return (
-    <a className="ticketLink" href={gig.ticketLink} target="_blank" rel="noreferrer">{gig.date} - {gig.name}</a>
-  );
-});
+import useScript from "../hooks/useScript";
+import "./Live.css";
 
 const Live = () => {
   return (
@@ -15,7 +9,28 @@ const Live = () => {
       <Helmet>
         <title>Live | Pyncher</title>
       </Helmet>
-      {gigList}
+      <a
+        href="https://www.songkick.com/artists/10177018"
+        class="songkick-widget"
+        data-theme="dark"
+        data-track-button="on"
+        data-detect-style="off"
+        data-background-color="rgb(0,0,0,1)"
+        data-font-color="rgb(255,255,255,1)"
+        data-button-bg-color="rgb(255,255,255,1)"
+        data-button-text-color="rgb(0,0,0,1)"
+        data-locale="en"
+        data-other-artists="on"
+        data-share-button="on"
+        data-country-filter="on"
+        data-rsvp="on"
+        data-request-show="on"
+        data-past-events="off"
+        data-past-events-offtour="off"
+        data-remind-me="off"
+        style={{ display: "none" }}
+      ></a>
+      {useScript("//widget-app.songkick.com/injector/10177018")}
     </React.Fragment>
   );
 };
