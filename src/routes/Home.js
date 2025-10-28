c6ddf9d7692324e447d6b2c5dd75375efb194a7f
George Hanlon <georgehanlon@live.com>
2025-10-28 22:22:18 +0000

    update meta tags and add album link

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,6 +9,17 @@ const Home = () => {
       <Helmet>
         <title>Pyncher</title>
       </Helmet>
+      <p className="aboutText" style={{ maxWidth: "60%" }}>
+        New album out now:{" "}
+        <a
+          href="https://ditto.fm/every-town-needs-a-stranger"
+          target="_blank"
+          rel="noreferrer"
+          className="ticketLink"
+        >
+          Every Town Needs a Stranger
+        </a>
+      </p>
       <img
         className="homeImage"
         src={pyncherImage}
